bfb91c8ca8a4db89a9c3cb75855c9c3269168ad6
russelldelgdo <rjdelgado@deditec.es>
2021-10-07 11:44:09 +0200

    terminado formularios reactivos apartado de dinamicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario : FormGroup = new FormGroup({
+  //   nombre      : new FormControl('russell formulario'),
+  //   precio      : new FormControl(1400),
+  //   existencias : new FormControl(7)
+
+  // });
+
+  //este formulario es igual que el de arriba pero mas sencillo para que pueda ser ampliable
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]], // primero es el valor por defecto , luego validaciones y posteriormente validaciones asyncronas.
+    precio: [, [Validators.required, Validators.min(0)]], // primero es el valor por defecto , luego validaciones y posteriormente validaciones asyncronas.
+    existencias: [, [Validators.required, Validators.minLength(3)]], // primero es el valor por defecto , luego validaciones y posteriormente validaciones asyncronas.
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    //esto lo hacemos para indicarle valores por defecto nuestro formulario cuando haya sido creado , 
+    //ahora bien si no vienen algun valor indicado entonces mi formulario revienta
+
+
+    this.miFormulario.reset({
+      nombre: "russsell",
+        precio : 200,
+    });
+
+  }
+
+
+
+
+  campoEsValido(campo: string) {
+    //con eso le paso el valor de la propiedad a verificar , si le paso nombre comprobara nombre , si le paso precio pues el precio y asi sucesivamente.
+
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched(); //esto hace que hicieramos como si todo el formulario haya sido tocado.
+      return;
+    }
+
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
   }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario : FormGroup = this.fb.group({
+    nombre : ['', [Validators.required , Validators.minLength(5)]],
+    favoritos : this.fb.array([
+      //estos que estan dentro no son arreglos si no instancias de form control.
+      ['russell' , [Validators.required] ], //this.fb.control...
+      ['controlador 2' , [Validators.required]],
+
+    ] , [Validators.required] , [])
+  });
+
+
+  nuevoFavorito : FormControl = new FormControl( '' , [Validators.required]);
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray; //indico que me traiga todos mis favoritos como un forma array
+  }
+
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+   // this.miFormulario.reset();
+  }
+
+
+
+  validar(valor : string){
+    return this.miFormulario.controls[valor].touched && this.miFormulario.controls[valor].errors;
+  
+  }
+
+
+    borrar(index : number){
+    console.log(`borrando : ${index}`);
+
+      this.favoritosArr.removeAt(index);
+    
+  }
+
+
+  agregarFavorito(){
+
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+
+
+    this.favoritosArr.push(new FormControl(this.nuevoFavorito.value))
+   // this.nuevoFavorito.reset();
+
+    console.log("agregar")
   }
 
 }
